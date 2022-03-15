f837ce998348b5dee5e40cb4aa26cae0d69e3d37
Przemek Luniewski <33295482+fosterer@users.noreply.github.com>
2022-03-15 19:35:12 +0100

    fixes for test

--- __mocks__/handlers.js
+++ __mocks__/handlers.js
@@ -1,8 +1,16 @@
 import { graphql } from "msw";
-import exampleData from "./api-response-1.json";
+import charactersTestData from "./api-characters-1.json";
+import episodesTestData from "./api-episodes-1.json";
+import locationsTestData from "./api-locations-1.json";
 
 export const handlers = [
-  graphql.query("myQuery", (req, res, ctx) => {
-    return res(ctx.data(exampleData));
+  graphql.query("useCharactersDataQuery", (req, res, ctx) => {
+    return res(ctx.data(charactersTestData));
+  }),
+  graphql.query("useEpisodesDataQuery", (req, res, ctx) => {
+    return res(ctx.data(episodesTestData));
+  }),
+  graphql.query("useLocationsDataQuery", (req, res, ctx) => {
+    return res(ctx.data(locationsTestData));
   }),
 ];
---
+++ __tests__/e2e-Lists.test.tsx
@@ -0,0 +1,37 @@
+import { screen, render, waitFor } from "@testing-library/react";
+import apolloClient from "@/lib/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { ListCharacters, ListEpisodes, ListLocations } from "@/features/lists";
+
+test("ListCharacters renders inline with snapshot", async () => {
+  const { container } = render(
+    <ApolloProvider client={apolloClient}>
+      <ListCharacters />
+    </ApolloProvider>
+  );
+
+  await waitFor(() => screen.getByText("TEST"));
+  expect(container).toMatchSnapshot();
+});
+
+test("ListEpisodes renders inline with snapshot", async () => {
+  const { container } = render(
+    <ApolloProvider client={apolloClient}>
+      <ListEpisodes />
+    </ApolloProvider>
+  );
+
+  await waitFor(() => screen.getByText("TEST"));
+  expect(container).toMatchSnapshot();
+});
+
+test("ListLocations renders inline with snapshot", async () => {
+  const { container } = render(
+    <ApolloProvider client={apolloClient}>
+      <ListLocations />
+    </ApolloProvider>
+  );
+
+  await waitFor(() => screen.getByText("TEST"));
+  expect(container).toMatchSnapshot();
+});
